db267a90d14dda45fb5b9b1cc73a0dd4c13dc7fe
supermanmwg <554607289@qq.com>
2021-07-01 20:19:57 +0800

    ADD:init

---
+++ OpenGL/OpenGL/main.cpp
@@ -0,0 +1,78 @@
+//  main.cpp
+//  OpenGL
+//
+//  Created by supermanmwng on 2021/6/19.
+//  
+//
+//         .-"-.
+//       _/_-.-_\_
+//      / __> <__ \
+//     / //  "  \\ \   see no evil...
+//    / / \'---'/ \ \
+//    \ \_/`"""`\_/ /
+//     \           /
+
+#define  GL_SILENCE_DEPRECATION
+#include "glew.h"
+#include "glfw3.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    if (glewInit() != GLEW_OK) {
+        std::cout << "glew init failed" << std::endl;
+        return -1;
+    }
+    
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
