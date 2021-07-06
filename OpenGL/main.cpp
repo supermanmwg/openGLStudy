8fa27420f5b60157dd525849aab7a5c62bd222e6
supermanmwg <554607289@qq.com>
2021-07-06 12:44:14 +0800

    MOD:使用index buffer生成一个矩形；

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -127,23 +127,30 @@ int main(void)
         -0.5f, -0.5f,
         -0.5f,  0.5f,
          0.5f,  0.5f,
-         0.5f,  0.5f,
          0.5f, -0.5f,
-        -0.5f, -0.5f
     };
-
-    unsigned int buffer, vertexArray;
-    glGenVertexArrays(1, &vertexArray);
-    glGenBuffers(1, &buffer);
-    glBindVertexArray(vertexArray);
     
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    
+    unsigned int vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     
-//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
     
     ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
     cout << "vertex" << endl;
@@ -153,7 +160,6 @@ int main(void)
     cout << source.FragmentSource << endl;
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
-    glBindVertexArray(vertexArray);
     
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -162,7 +168,7 @@ int main(void)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -171,6 +177,7 @@ int main(void)
         glfwPollEvents();
     }
 
+    glDeleteShader(shader);
     glfwTerminate();
     return 0;
 }
